e6a8d8b5704e3a8834f01f9a81e18342883e8e12
Ahad2008 <72267510+Ahad2008@users.noreply.github.com>
2021-02-18 14:14:10 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,92 +1,115 @@
-var backImage,backgr;
+var backImage,jungle;
 var player, player_running;
 var ground,ground_img;
-var FoodGroup, ObstaclesGroup;
-
+var FoodGroup, obstacleGroup;
 var END =0;
 var PLAY =1;
-var gameState = PLAY;
 var score = 0;
+var gameState = PLAY;
+var banana,bananaImage;
+var obstacle,obstacleImage;
+var gameOver,gameOverImg;
 
 function preload(){
   backImage=loadImage("jungle.jpg");
   player_running = loadAnimation("Monkey_01.png","Monkey_02.png","Monkey_03.png","Monkey_04.png","Monkey_05.png","Monkey_06.png","Monkey_07.png","Monkey_08.png","Monkey_09.png","Monkey_10.png");
   bananaImage = loadImage("banana.png");
-  stoneImage = loadImage("stone.png");
+  obstacleImage = loadImage("stone.png");
+  gameOverImg = loadImage("gameOver.png");
 }
 
 function setup() {
   createCanvas(800,400);
   
-  backgr=createSprite(0,0,800,400);
-  backgr.addImage(backImage);
-  backgr.scale=1.5;
-  backgr.x=backgr.width/2;
-  backgr.velocityX=-4;
+  jungle=createSprite(0,0,800,400);
+  jungle.addImage(backImage);
+  jungle.scale=1.5;
+  jungle.x=jungle.width/2;
+  jungle.velocityX=-4;
   
-  player = createSprite(100,340,20,50);
+  player = createSprite(100,380,20,50);
   player.addAnimation("Running",player_running);
   player.scale = 0.1;
   
-  ground = createSprite(400,350,800,10);
+  ground = createSprite(400,380,800,10);
   ground.x=ground.width/2;
-  ground.visible=false;
+  ground.visible = false;
+
+  gameOver = createSprite(400,200);
+  gameOver.addImage(gameOverImg);
+  gameOver.scale = 0.5;
+  gameOver.visible = false;
   
+  FoodGroup = createGroup();
+  obstacleGroup = createGroup(); 
 }
 
 function draw() { 
   background(0);
-
-  if(gameState===PLAY){
-
-    spawnFood();
-    spawnObstacles();
-
-    if (FoodGroup.isTouching(player)){
-      FoodGroup.destroyEach();
-      score = score + 2;
-      player.scale += + 0.1;
-    }
-
-  if(backgr.x<100){
-    backgr.x=backgr.width/2;
+  if(gameState === PLAY){
+  
+  if(jungle.x<100){
+    jungle.x=jungle.width/2;
   }
   
-    if(keyDown("space") ) {
+  if(keyDown("space") ) {
       player.velocityY = -12;
     }
     player.velocityY = player.velocityY + 0.8;
   
     player.collide(ground);
 
+  if(FoodGroup. isTouching(player)){
+    FoodGroup.destroyEach();
+    score = score + 5;
+    player.scale += 0.01;
   }
-
- // text("Score:" + score, 50, 750);
-
+  
+  if(obstacleGroup . isTouching(player)) {
+    gameState = END;
+  }
+  spawnFood()
+  spawnObstacle ()
   drawSprites();
+}
+  
+else if (gameState === END) {
+  gameOver.visible = true;
+  jungle.velocityX = 0;
+  player.velocityY = 0;
+  obstacleGroup.setVelocityXEach(0);
+  FoodGroup.setVelocityXEach(0);
+  obstacleGroup.setLifetimeEach(-1);
+  FoodGroup.setLifetimeEach(-1); 
+}
+
+drawSprites();
+  stroke("black");
+  textSize(20);
+  fill("white");
+  text("Score: "+score,700,50);
 }
 
 function spawnFood(){
-  if (frameCount % 80 === 0){
-    var banana = createSprite(600, 200, 40, 10);
-    banana.y = random(100, 150);
-    banana.addImage(bananaImage);
-    banana.scale = 0.05;
-    banana.velocityX = -4;
-    banana.lifetime = 300;
-    player.depth = banana.depth + 1;
-    FoodGroup.add(banana);
+  if (frameCount % 60 === 0) {
+  banana = createSprite(850,330,330,40);
+  banana.y = Math.round(random(120,200));
+  banana.addImage(bananaImage);
+  banana.scale = 0.05; 
+  banana.velocityX = -4;
+  banana.lifetime = 300;
+  FoodGroup.add(banana);
+  player.depth = banana.depth+1;
   }
 }
 
-function spawnObstacles(){
-  if (frameCount % 100 === 0){
-    var stone = createSprite(600, 320, 40, 10);
-    stone.addImage(stoneImage);
-    stone.scale = 0.2;
-    stone.velocityX = -4;
-    stone.lifetime = 300;
-    player.depth = stone.depth + 1;
-    ObstaclesGroup.add(stone);
+function spawnObstacle (){
+  if(frameCount % 150 === 0){
+    var obstacle = createSprite(850,350,20,20);
+    obstacle.addImage(obstacleImage);
+    obstacle.scale = 0.2;
+    obstacle.velocityX =-4;
+    obstacle.lifeTime = 10;
+    obstacleGroup.add(obstacle);
   }
 }
